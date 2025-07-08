1edf4d4bd682ed8e5101dc924a8b9b849fb5c175
mrkianiam <kianimehr.office@gmail.com>
2025-07-08 22:00:54 +0330

    Add files via upload

---
+++ petshop-voting-full-app/README.md
@@ -0,0 +1,2 @@
+# Petshop Voting App
+This is a simple Create React App setup for a voting app.
---
+++ petshop-voting-full-app/src/App.js
@@ -0,0 +1,17 @@
+
+export default function App() {
+  return (
+    <div style={{
+      backgroundColor: '#111',
+      color: 'white',
+      minHeight: '100vh',
+      fontFamily: 'sans-serif',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '24px'
+    }}>
+      اپلیکیشن رأی‌گیری اسم پت‌شاپ در حال توسعه است...
+    </div>
+  );
+}
